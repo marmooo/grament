68bf9905c9fa0a37f4a731caa219f9ed563a168a
marmooo <marmooo@users.noreply.github.com>
2022-07-24 09:06:15 +0900

    bump bootstrap from 5.1.3 to 5.2.0

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2022-06-03 09:20';
+var CACHE_NAME = "2022-07-24 00:46";
 var urlsToCache = [
   "/grament/",
   "/grament/index.js",
@@ -10,7 +10,7 @@ var urlsToCache = [
   "/grament/index.js",
   "/grament/favicon/favicon.svg",
   "https://marmooo.github.io/fonts/textar-light.woff2",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css",
 ];
 
 self.addEventListener("install", function (event) {
