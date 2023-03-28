ec23d6933d8bf43b3abb479f9351a94cdc9e3f0b
marmooo <marmooo@users.noreply.github.com>
2023-03-29 00:00:04 +0900

    cleanup

--- src/sw.js
+++ src/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "2023-02-16 09:45";
+var CACHE_NAME = "2023-03-29 00:00";
 var urlsToCache = [
   "/grament/",
   "/grament/index.js",
   "/grament/mp3/bgm.mp3",
   "/grament/mp3/cat.mp3",
-  "/grament/mp3/correct.mp3",
+  "/grament/mp3/correct3.mp3",
   "/grament/mp3/end.mp3",
   "/grament/mp3/keyboard.mp3",
   "/grament/index.js",
