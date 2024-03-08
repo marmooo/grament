4652dc7c91d8375fa93eab86ca26e3e7b03b5d54
marmooo <marmooo@users.noreply.github.com>
2024-03-08 09:37:35 +0900

    use globalThis

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ let errorCount = 0;
 let mistaken = false;
 let problems = [];
 let englishVoices = [];
-const audioContext = new AudioContext();
+const audioContext = new globalThis.AudioContext();
 const audioBufferCache = {};
 loadAudio("keyboard", "mp3/keyboard.mp3");
 loadAudio("end", "mp3/end.mp3");
@@ -152,7 +152,7 @@ loadVoices();
 
 function loopVoice(text, n) {
   speechSynthesis.cancel();
-  const msg = new SpeechSynthesisUtterance(text);
+  const msg = new globalThis.SpeechSynthesisUtterance(text);
   msg.voice = englishVoices[Math.floor(Math.random() * englishVoices.length)];
   msg.lang = "en-US";
   for (let i = 0; i < n; i++) {
